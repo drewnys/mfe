b93dd08ff146a5d4edfd36b8adea733d791a5fef
DREW NYS <drew.nys@gmail.com>
2023-03-13 15:32:41 -0500

    Adjust marketing yml

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
-        publicPath: '/marketing/latests/',
+        publicPath: '/marketing/latest/',
     },
     plugins: [
         new ModuleFederationPlugin({
