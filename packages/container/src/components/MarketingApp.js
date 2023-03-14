a23e132930410fd3244cd9fa13c727024b90efcd
DREW NYS <drew.nys@gmail.com>
2023-03-14 15:10:04 -0500

    Add bidirectional navigation routing

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,13 +1,26 @@
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { mount } from 'marketing/MarketingApp';
 
 export default () => {
     const ref = useRef(null);
+    const history = useHistory();
 
     // useEffect ensures that the code runs only a single time
     useEffect(()=> {
-        mount(ref.current);
-    });
+        const { onParentNavigate } = mount(ref.current, {
+            // Listen for pathname change in marketing module
+            onNavigate: ({ pathname: nextPathname }) => {
+                const { pathname } = history.location;
+
+                if (pathname !== nextPathname){
+                    history.push(nextPathname);
+                }
+            }
+        });
+
+        history.listen(onParentNavigate);
+    }, []); // Empty array [] to indicate to only run this function when MarketingApp is first rendered
     
     return <div ref={ref} />;
 };
